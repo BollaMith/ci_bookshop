777e3d1d5ba9b1fc0134bb2947ab8b2d45b81bd1
Bolla Mith <41566355+BollaMith@users.noreply.github.com>
2018-07-23 15:09:40 +0700

    update

--- application/views/bootstrap.php
+++ application/views/bootstrap.php
@@ -18,11 +18,13 @@
   
 <div class="container">
   <div class="row">
+
     <div class="col-sm-4">
       <h3><?php echo $col1; ?></h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
+
     <div class="col-sm-4">
       <h3><?php echo $col2; ?></h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
@@ -33,6 +35,8 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
+
+    
   </div>
 </div>
 
