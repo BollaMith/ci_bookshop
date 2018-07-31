8d201462e42af9f672091c846501d43dde67eb3f
SENG Sourng <sourng2009@gmail.com>
2018-07-31 18:09:53 +0700

    Update Detail

---
+++ application/views/front/detail/product_image.php
@@ -0,0 +1,241 @@
+<div class="center_column col-xs-12 col-sm-9" id="center_column">
+                <!-- Product -->
+                    <div id="product">
+                        <div class="primary-box row">
+                        <div class="pb-left-column col-xs-12 col-sm-6">
+                                 <!-- product-imge-->
+                                <div class="product-image">
+                                    <div class="product-full">
+                                        <img id="product-zoom" src='<?php echo base_url();?>uploads/products/p2.jpg' data-zoom-image="<?php echo base_url();?>uploads/products/p2.jpg"/>
+                                    </div>
+                                    <div class="product-img-thumb" id="gallery_01">
+                                        <ul class="owl-carousel" data-items="3" data-nav="true" data-dots="false" data-margin="20" data-loop="true">
+                                            											
+											<?php
+											for($i=1;$i<6;$i++){
+												?>
+												<li>
+                                                <a href="#" data-image="<?php echo base_url();?>uploads/products/p<?php echo $i; ?>.jpg" data-zoom-image="<?php echo base_url();?>uploads/products/p<?php echo $i; ?>.jpg">
+                                                    <img id="product-zoom"  src="<?php echo base_url();?>uploads/products/p<?php echo $i; ?>.jpg" /> 
+                                                </a>
+											</li>
+												<?php
+											}											
+											?>	
+
+                                        </ul>
+                                    </div>
+									
+									
+                                </div>
+                                <!-- product-imge-->
+                            </div>
+                         
+                         
+                            <div class="pb-right-column col-xs-12 col-sm-6">
+                                <h1 class="product-name">Maecenas consequat mauris</h1>
+                                <div class="product-comments">
+                                    <div class="product-star">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-half-o"></i>
+                                    </div>
+                                    <div class="comments-advices">
+                                        <a href="#">Based  on 3 ratings</a>
+                                        <a href="#"><i class="fa fa-pencil"></i> write a review</a>
+                                    </div>
+                                </div>
+                                <div class="product-price-group">
+                                    <span class="price">$38.95</span>
+                                    <span class="old-price">$52.00</span>
+                                    <span class="discount">-30%</span>
+                                </div>
+                                <div class="info-orther">
+                                    <p>Item Code: #453217907</p>
+                                    <p>Availability: <span class="in-stock">In stock</span></p>
+                                    <p>Condition: New</p>
+                                </div>
+                                <div class="product-desc">
+                                    Vestibulum eu odio. Suspendisse potenti. Morbi mollis tellus ac sapien. Praesent egestas tristique nibh. Nullam dictum felis eu pede mollis pretium.Fusce egestas elit eget lorem. 
+                                </div>
+                                <div class="form-option">
+                                    <p class="form-option-title">Available Options:</p>
+                                    <div class="attributes">
+                                        <div class="attribute-label">Color:</div>
+                                        <div class="attribute-list">
+                                            <ul class="list-color">
+                                                <li style="background:#0c3b90;"><a href="#">red</a></li>
+                                                <li style="background:#036c5d;" class="active"><a href="#">red</a></li>
+                                                <li style="background:#5f2363;"><a href="#">red</a></li>
+                                                <li style="background:#ffc000;"><a href="#">red</a></li>
+                                                <li style="background:#36a93c;"><a href="#">red</a></li>
+                                                <li style="background:#ff0000;"><a href="#">red</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="attributes">
+                                        <div class="attribute-label">Qty:</div>
+                                        <div class="attribute-list product-qty">
+                                            <div class="qty">
+                                                <input id="option-product-qty" type="text" value="1">
+                                            </div>
+                                            <div class="btn-plus">
+                                                <a href="#" class="btn-plus-up">
+                                                    <i class="fa fa-caret-up"></i>
+                                                </a>
+                                                <a href="#" class="btn-plus-down">
+                                                    <i class="fa fa-caret-down"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="attributes">
+                                        <div class="attribute-label">Size:</div>
+                                        <div class="attribute-list">
+                                            <select>
+                                                <option value="1">X</option>
+                                                <option value="2">XL</option>
+                                                <option value="3">XXL</option>
+                                            </select>
+                                            <a id="size_chart" class="fancybox" href="<?php echo base_url(); ?>uploads/products/p5.jpg">Size Chart</a>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-action">
+                                    <div class="button-group">
+                                        <a class="btn-add-cart" href="#">Add to cart</a>
+                                    </div>
+                                    <div class="button-group">
+                                        <a class="wishlist" href="#"><i class="fa fa-heart-o"></i>
+                                        <br>Wishlist</a>
+                                        <a class="compare" href="#"><i class="fa fa-signal"></i>
+                                        <br>        
+                                        Compare</a>
+                                    </div>
+                                </div>
+                                <div class="form-share">
+                                    <div class="sendtofriend-print">
+                                        <a href="javascript:print();"><i class="fa fa-print"></i> Print</a>
+                                        <a href="#"><i class="fa fa-envelope-o fa-fw"></i>Send to a friend</a>
+                                    </div>
+                                    <div class="network-share">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- tab product -->
+                        <div class="product-tab">
+                            <ul class="nav-tab">
+                                <li class="active">
+                                    <a aria-expanded="false" data-toggle="tab" href="#product-detail">Product Details</a>
+                                </li>
+                                <li>
+                                    <a aria-expanded="true" data-toggle="tab" href="#information">information</a>
+                                </li>
+                                <li>
+                                    <a data-toggle="tab" href="#reviews">reviews</a>
+                                </li>
+                                <li>
+                                    <a data-toggle="tab" href="#extra-tabs">Extra Tabs</a>
+                                </li>
+                                <li>
+                                    <a data-toggle="tab" href="#guarantees">guarantees</a>
+                                </li>
+                            </ul>
+                            <div class="tab-container">
+                                <div id="product-detail" class="tab-panel active">
+                                    <p>Morbi mollis tellus ac sapien. Nunc nec neque. Praesent nec nisl a purus blandit viverra. Nunc nec neque. Pellentesque auctor neque nec urna.</p>
+
+                                    <p>Curabitur suscipit suscipit tellus. Cras id dui. Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Maecenas vestibulum mollis diam.</p>
+
+                                    <p>Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Sed lectus. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Nam at tortor in tellus interdum sagittis. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est.</p>
+                                </div>
+                                <div id="information" class="tab-panel">
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <td width="200">Compositions</td>
+                                            <td>Cotton</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Styles</td>
+                                            <td>Girly</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Properties</td>
+                                            <td>Colorful Dress</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div id="reviews" class="tab-panel">
+                                    <div class="product-comments-block-tab">
+                                        <div class="comment row">
+                                            <div class="col-sm-3 author">
+                                                <div class="grade">
+                                                    <span>Grade</span>
+                                                    <span class="reviewRating">
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                    </span>
+                                                </div>
+                                                <div class="info-author">
+                                                    <span><strong>Jame</strong></span>
+                                                    <em>04/08/2015</em>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-9 commnet-dettail">
+                                                Phasellus accumsan cursus velit. Pellentesque egestas, neque sit amet convallis pulvinar
+                                            </div>
+                                        </div>
+                                        <div class="comment row">
+                                            <div class="col-sm-3 author">
+                                                <div class="grade">
+                                                    <span>Grade</span>
+                                                    <span class="reviewRating">
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                    </span>
+                                                </div>
+                                                <div class="info-author">
+                                                    <span><strong>Author</strong></span>
+                                                    <em>04/08/2015</em>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-9 commnet-dettail">
+                                                Phasellus accumsan cursus velit. Pellentesque egestas, neque sit amet convallis pulvinar
+                                            </div>
+                                        </div>
+                                        <p>
+                                            <a class="btn-comment" href="#">Write your review !</a>
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                                <div id="extra-tabs" class="tab-panel">
+                                    <p>Phasellus accumsan cursus velit. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Sed lectus. Sed a libero. Vestibulum eu odio.</p>
+
+                                    <p>Maecenas vestibulum mollis diam. In consectetuer turpis ut velit. Curabitur at lacus ac velit ornare lobortis. Praesent ac sem eget est egestas volutpat. Nam eget dui.</p>
+
+                                    <p>Maecenas nec odio et ante tincidunt tempus. Vestibulum suscipit nulla quis orci. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aenean ut eros et nisl sagittis vestibulum. Aliquam eu nunc.</p> 
+                                </div>
+                                <div id="guarantees" class="tab-panel">
+                                    <p>Phasellus accumsan cursus velit. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Sed lectus. Sed a libero. Vestibulum eu odio.</p>
+
+                                    <p>Maecenas vestibulum mollis diam. In consectetuer turpis ut velit. Curabitur at lacus ac velit ornare lobortis. Praesent ac sem eget est egestas volutpat. Nam eget dui.</p>
+
+                                    <p>Maecenas nec odio et ante tincidunt tempus. Vestibulum suscipit nulla quis orci. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aenean ut eros et nisl sagittis vestibulum. Aliquam eu nunc.</p> 
+                                    <p>Maecenas vestibulum mollis diam. In consectetuer turpis ut velit. Curabitur at lacus ac velit ornare lobortis. Praesent ac sem eget est egestas volutpat. Nam eget dui.</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                                
--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -144,65 +144,8 @@
                         <li><a href="#" >Smart TV</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-12">
-                    <ul id="trademark-mobile-list" class="trademark-list">
-                        <li class="trademark-text-tit">MOBILES:</li>  
-                        <li><a href="#" >Moto E</a></li>
-                        <li><a href="#" >Samsing Mobile</a></li>
-                        <li><a href="#" >Micromaxi Mobile</a></li>
-                        <li><a href="#" >Nokian Mobile</a></li>
-                        <li><a href="#" >HTCi Mobile</a></li>
-                        <li><a href="#" >Sonyo Mobile</a></li>
-                        <li><a href="#" >Appleo Mobile</a></li>
-                        <li><a href="#" >LGG Mobile</a></li>
-                        <li><a href="#" >Karbonni Mobile</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-12">
-                    <ul id="trademark-laptop-list" class="trademark-list">
-                        <li class="trademark-text-tit">LAPTOPS::</li> 
-                        <li><a href="#" >Appleo Laptop</a></li>
-                        <li><a href="#" >Acer Laptop</a></li>
-                        <li><a href="#" >Samsing Laptop</a></li>
-                        <li><a href="#" >Lenov Laptop</a></li>
-                        <li><a href="#" >Sonyo Laptop</a></li>
-                        <li><a href="#" >Delli Laptop</a></li>
-                        <li><a href="#" >Asuso Laptop</a></li>
-                        <li><a href="#" >Toshibao Laptop</a></li>
-                        <li><a href="#" >LGG Laptop</a></li>
-                        <li><a href="#" >HPo Laptop</a></li>
-                        <li><a href="#" >Notebook</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-12">
-                    <ul id="trademark-watches-list" class="trademark-list">
-                        <li class="trademark-text-tit">WATCHES:</li>  
-                        <li><a href="#" >FCUKo Watches</a></li>
-                        <li><a href="#" >Titan Watches</a></li>
-                        <li><a href="#" >Casioo Watches</a></li>
-                        <li><a href="#" >Fastrack Watches</a></li>
-                        <li><a href="#" >Timexi Watches</a></li>
-                        <li><a href="#" >Fossili Watches</a></li>
-                        <li><a href="#" >Dieselo Watches</a></li>
-                        <li><a href="#" >Toshibao Laptop</a></li>
-                        <li><a href="#" >Luxury Watches</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-12">
-                    <ul id="trademark-shoes-list" class="trademark-list">
-                        <li class="trademark-text-tit">FOOTWEAR:</li>  
-                        <li><a href="#" >Shoes</a></li>
-                        <li><a href="#" >Casual Shoes</a></li>
-                        <li><a href="#" >Sports Shoes</a></li>
-                        <li><a href="#" >Adidasi Shoes</a></li>
-                        <li><a href="#" >Gas Shoes</a></li>
-                        <li><a href="#" >Pumai Shoes</a></li>
-                        <li><a href="#" >Reeboki Shoes</a></li>
-                        <li><a href="#" >Woodlandi Shoes</a></li>
-                        <li><a href="#" >Red tape Shoes</a></li>
-                        <li><a href="#" >Nikeo Shoes</a></li>
-                    </ul>
-                </div>
+             
+            
             </div>
             <!-- /#trademark-text-box -->
             <div id="footer-menu-box">
@@ -222,36 +165,9 @@
                         <li><a href="#" >Support</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-12">
-                    <ul class="footer-menu-list">
-                        <li><a href="#" >Most Populars</a></li>
-                        <li><a href="#" >Best Sellers</a></li>
-                        <li><a href="#" >New Arrivals</a></li>
-                        <li><a href="#" >Special Products</a></li>
-                        <li><a href="#" >Manufacturers</a></li>
-                        <li><a href="#" >Our Stores</a></li>
-                        <li><a href="#" >Shipping</a></li>
-                        <li><a href="#" >Payments</a></li>
-                        <li><a href="#" >Warantee</a></li>
-                        <li><a href="#" >Refunds</a></li>
-                        <li><a href="#" >Checkout</a></li>
-                        <li><a href="#" >Discount</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-12">
-                    <ul class="footer-menu-list">
-                        <li><a href="#" >Terms & Conditions</a></li>
-                        <li><a href="#" >Policy</a></li>
-                        <li><a href="#" >Shipping</a></li>
-                        <li><a href="#" >Payments</a></li>
-                        <li><a href="#" >Returns</a></li>
-                        <li><a href="#" >Refunds</a></li>
-                        <li><a href="#" >Warrantee</a></li>
-                        <li><a href="#" >FAQ</a></li>
-                        <li><a href="#" >Contact</a></li>
-                    </ul>
-                </div>
-                <p class="text-center">Copyrights &#169; 2015 KuteShop. All Rights Reserved. Designed by KuteThemes.com</p>
+         
+              
+                <p class="text-center">Copyrights &#169; 2018 Sourng.COM. All Rights Reserved. Designed by Sourng Co.Ltd </p>
             </div>
             <!-- /#footer-menu-box -->
         </div> 
